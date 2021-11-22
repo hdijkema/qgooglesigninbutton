c278c1e76313b3aa9fb935b86c7549bbf96f6e64
Hans Dijkema <hans@dijkewijk.nl>
2021-11-22 23:35:39 +0100

    LGPL Notice
    Signed-off-by: Hans Dijkema <hans@dijkewijk.nl>

--- qgooglesigninbutton_global.h
+++ qgooglesigninbutton_global.h
@@ -1,6 +1,25 @@
 #ifndef QGOOGLESIGNINBUTTON_GLOBAL_H
 #define QGOOGLESIGNINBUTTON_GLOBAL_H
 
+/*
+ * This file is part of QGoogleSignInButton.
+ *
+ * (c) Hans Dijkema 2021
+ *
+ *  QGoogleSignInButton is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 2.1 of the License, or
+ *  (at your option) any later version.
+ *
+ *  QGoogleSignInButton is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with QGoogleSignInButton. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include <QtCore/qglobal.h>
 
 #if defined(QGOOGLESIGNINBUTTON_TEST)
